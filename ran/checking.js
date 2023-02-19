8753bec1fd2f76e0fdc2ed76422efcc0b57d796e
Aditisadadiwala <113763395+Aditisadadiwala@users.noreply.github.com>
2023-02-19 10:07:33 +0400

    Delete ran directory

--- ran/checking.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = new express()
-const path =  require('path')
-
-console.log(path.join(process.cwd(),'../client','public','index.html'))
-console.log(path.join(process.cwd(),'../client','src'))
